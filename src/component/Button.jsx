5df92edb175fef3d872883017ab1a72b8df8d77a
Ankit Kumar <ankkrs@gmail.com>
2023-10-21 22:23:48 +0530

    reverse

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -5,7 +5,7 @@ export default function Button() {
   // const PDF = "../Image"
   return (
 
-    <a target='_blank' className='text-decoration-none' href={PDF} id="resume-link-2">
+    <a target='_blank' className='text-decoration-none' href={PDF} id="resume-button-2">
     <div>
       <button className="button " type="button">
 <span className="button__text">Resume</span>
