df1231473eea4d5b4450fc6b30707744feb2547a
Ankit Kumar <ankkrs@gmail.com>
2023-10-21 22:20:02 +0530

    resume link

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -71,7 +71,7 @@ export default function () {
               </a>
             </li>
             <li className="nav-item me-lg-5">
-              <a className="nav-link resume fw-bold text-black" target='_blank' href={PDF} id="resume-button-1">
+              <a className="nav-link resume fw-bold text-black" target='_blank' href={PDF} id="resume-link-1">
                 Resume
               </a>
             </li>
